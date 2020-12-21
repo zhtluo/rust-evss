bdbc07219fa3df7612b1ab89c188d86c92038283
zhtluo <zhtluo@gmail.com>
2020-12-21 14:11:52 -0500

    test

--- src/evss.rs
+++ src/evss.rs
@@ -177,25 +177,42 @@ mod tests {
     type Poly381 = DensePolynomial<F381>;
     type PC381 = MarlinKZG10<Bls12_381, Poly381>;
     type EVSS381 = evss::EVSS<F381, Poly381, PC381>;
+    type EVSSParams381 = evss::EVSSParams<F381, Poly381, PC381>;
+    type EVSSPublicParams381 = evss::EVSSPublicParams<F381, Poly381, PC381>;
+    type EVSSShare381 = evss::EVSSShare<F381, Poly381, PC381>;
 
     const DEGREE: usize = 10;
     const INDEX_BEGIN: usize = 1;
 
     #[test]
-    fn test() -> Result<(), <PC381 as PolynomialCommitment<F381, Poly381>>::Error> {
+    fn test_functionality() -> Result<(), <PC381 as PolynomialCommitment<F381, Poly381>>::Error> {
         let rng = &mut test_rng();
         let secret = F381::rand(rng);
         let params = EVSS381::setup(secret, DEGREE, rng)?;
         let mut shares = Vec::new();
         for i in INDEX_BEGIN..INDEX_BEGIN + DEGREE + 1 {
-            let serialized = serde_json::to_string(&EVSS381::get_share(F381::from(i as u32), &params, rng)?).unwrap();
-            println!("{}", serialized);
-            shares.push(serde_json::from_str(&serialized).unwrap());
+            shares.push(EVSS381::get_share(F381::from(i as u32), &params, rng)?);
         }
         for sh in &shares {
             assert!(EVSS381::check(&params.get_public_params(), sh, rng)?);
         }
         assert_eq!(secret, EVSS381::reconstruct(&shares));
+        Ok(())
+    }
+
+    #[test]
+    fn test_serde() -> Result<(), serde_json::Error> {
+        let rng = &mut test_rng();
+        let secret = F381::rand(rng);
+        let params = EVSS381::setup(secret, DEGREE, rng).unwrap();
+        let _: EVSSParams381 = serde_json::from_str(&serde_json::to_string(&params)?)?;
+        let _: EVSSPublicParams381 = serde_json::from_str(&serde_json::to_string(&params.get_public_params())?)?;
+        for i in INDEX_BEGIN..INDEX_BEGIN + DEGREE + 1 {
+            println!("{}", serde_json::to_string(
+                    &EVSS381::get_share(F381::from(i as u32), &params, rng).unwrap())?);
+            let _: EVSSShare381 = serde_json::from_str(&serde_json::to_string(
+                    &EVSS381::get_share(F381::from(i as u32), &params, rng).unwrap())?)?;
+        }
         Ok(())
     }
 }
